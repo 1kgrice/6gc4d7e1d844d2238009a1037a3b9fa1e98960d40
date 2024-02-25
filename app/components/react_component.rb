d9fa3deabc47df3abf543f867ade81589a2bd6e0
Kirill Ragozin <ragozin@hey.com>
2024-02-25 21:02:55 +0300

    feat: allow setting inline style in react_component

--- app/components/react_component.rb
+++ app/components/react_component.rb
@@ -3,15 +3,16 @@
 class ReactComponent < ViewComponent::Base
   attr_reader :component, :raw_props, :html_class
 
-  def initialize(component, raw_props: {}, html_class: '')
+  def initialize(component, raw_props: {}, html_class: '', style: '')
     super
     @component = component
     @raw_props = raw_props
     @html_class = html_class
+    @style = style
   end
 
   def call
-    helpers.tag.div('', data: { react_component: component, props: props }, class: class_name)
+    helpers.tag.div('', data: { react_component: component, props: props }, class: class_name, style: @style)
   end
 
   private
